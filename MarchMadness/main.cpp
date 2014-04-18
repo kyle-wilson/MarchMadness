09cc1b1d20260ddf161f064dc35e33c80ec11516
kyle4w <kyle4w@gmail.com>
2014-04-18 19:04:15 -0400

    Initial Commit

---
+++ MarchMadness/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MarchMadness
+//
+//  Created by Kyle Wilson on 4/18/14.
+//  Copyright (c) 2014 Kyle Wilson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
